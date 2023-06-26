12e97c4cd1108cd9f594e9ae98bac143d2421f47
QubaPra <116064097+QubaPra@users.noreply.github.com>
2023-06-26 08:40:52 +0200

    backend 3

--- api/customers/router.py
+++ api/customers/router.py
@@ -1,10 +1,8 @@
-from fastapi import APIRouter, HTTPException, Query
-
+from fastapi import APIRouter, HTTPException
 from .storage import get_customers_storage
 from .schema import CustomerCreateSchema, CustomerUpdateSchema, Customer
 
 router = APIRouter()
-
 
 CUSTOMERS_STORAGE = get_customers_storage()
 
@@ -23,13 +21,19 @@ async def get_customer(customer_id: int) -> Customer:
             status_code=404, detail=f"Customer with ID={customer_id} does not exist."
         )
 
-
 @router.patch("/{customer_id}")
 async def update_customer(
     customer_id: int, updated_customer: CustomerUpdateSchema
 ) -> Customer:
-   pass
-
+    if customer_id not in CUSTOMERS_STORAGE:
+        raise HTTPException(
+            status_code=404, detail=f"Customer with ID={customer_id} does not exist."
+        )
+    customer = CUSTOMERS_STORAGE[customer_id]
+    updated_fields = updated_customer.dict(exclude_unset=True)
+    for field, value in updated_fields.items():
+        setattr(customer, field, value)
+    return customer
 
 @router.delete("/{customer_id}")
 async def delete_customer(customer_id: int) -> None:
@@ -40,7 +44,9 @@ async def delete_customer(customer_id: int) -> None:
             status_code=404, detail=f"Customer with ID={customer_id} does not exist."
         )
 
-
 @router.post("/")
 async def create_customer(customer: CustomerCreateSchema) -> Customer:
-   pass
+    customer_id = max(CUSTOMERS_STORAGE.keys(), default=0) + 1
+    new_customer = Customer(id=customer_id, **customer.dict())
+    CUSTOMERS_STORAGE[customer_id] = new_customer
+    return new_customer
